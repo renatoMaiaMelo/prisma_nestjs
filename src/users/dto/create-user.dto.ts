1d45add7756ed82c7cc71b0798f853bfbf6cd018
Renato Maia <renato171179@gmail.com>
2023-11-08 15:31:04 -0400

    first commit

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString, IsBoolean, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsBoolean()
+  admin: boolean;
+}
